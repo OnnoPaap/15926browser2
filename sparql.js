432c0750717a27dcd9b0f3d7f0a290b93e7adb18
Onno Paap <onno.paap@gmail.com>
2021-07-31 15:21:15 +0200

    test

--- sparql.js
+++ sparql.js
@@ -1,24 +1,22 @@
-
-
-var superagent = require('superagent');
+var superagent = require("superagent");
 
 var query = {
-    'query': 'select (count(*) as ?cnt) {?s ?p ?o}'
+  query: "select (count(*) as ?cnt) {?s ?p ?o}",
 };
 
 var headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Accept': 'application/sparql-results+json'
+  "Content-Type": "application/x-www-form-urlencoded",
+  Accept: "application/sparql-results+json",
 };
 
 superagent
-    .post('http://infoweb.ws/sparql')
-    .set(headers)
-    /* .auth('admin', 'admin') */
-    .send(query)
-    .end(function(err, response) {
-        console.log('Superagent:')
-        console.log('STATUS: ' + response.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(response.headers));
-        console.log('BODY: ' + response.text);
-    });
+  .post("http://infoweb.ws/sparql")
+  .set(headers)
+  /* .auth('admin', 'admin') */
+  .send(query)
+  .end(function (err, response) {
+    console.log("Superagent:");
+    console.log("STATUS: " + response.statusCode);
+    console.log("HEADERS: " + JSON.stringify(response.headers));
+    console.log("BODY: " + response.text);
+  });
